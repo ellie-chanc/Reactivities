0b49dffa1893dc52fe6e6fceaafda535d29435da
Ellie Chan <chanc@bhf.org.uk>
2026-05-28 16:43:43 +0100

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -4,17 +4,22 @@ using Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+// Adding order does not matter
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();  // so that react app can access the API
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// Middleware order matters
+app.UseCors(x => x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:3000", "https://localhost:3000"));
+
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
